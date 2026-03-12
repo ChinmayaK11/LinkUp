6077204d74ae73b67f095510679ab737a3e80a6d
Chinmaya Kagolli <chinmayak.mca24@rvce.edu.in>
2026-03-12 21:47:38 +0530

    Initial commit - LinkUp full stack social media app

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb=async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("DB connected")
+    } catch (error) {
+         console.error("DB error:", error.message);
+        console.error(error);
+    }
+}
+
+export default connectDb
