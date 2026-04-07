09a94188e66b9a4435f75ab93e97dcfe91b9acb9
Chinmaya Kagolli <chinmayak.mca24@rvce.edu.in>
2026-04-07 22:57:40 +0530

    unnecessary logs removed

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,                  // max 100 requests per window per IP
+    max: 100,                  
     standardHeaders: true,
     legacyHeaders: false,
     message: {
